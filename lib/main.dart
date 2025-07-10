7bc39ec097e15db7c8a8261902fbb9c51b8bc1b7
Syed Shayan <3syedshayan@gmail.com>
2025-07-10 23:28:20 +0500

    Theme Data added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
+final ColorScheme colorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 102, 6, 247),
+  brightness: Brightness.dark,
+  surface: Color.fromARGB(255, 56, 49, 66),
+);
+
+final ThemeData themeData = ThemeData().copyWith(
+  colorScheme: colorScheme,
+  scaffoldBackgroundColor: colorScheme.surface,
+  appBarTheme: AppBarTheme(
+    backgroundColor: colorScheme.primary,
+    foregroundColor: colorScheme.onPrimary,
+  ),
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleMedium: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+  ),
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -10,11 +31,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
