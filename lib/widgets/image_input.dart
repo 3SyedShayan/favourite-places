f5d59e18c238b061e4528f97a8a751e7d6f3ebd1
Syed Shayan <3syedshayan@gmail.com>
2025-07-17 17:02:56 +0500

    Image Picking from Camera

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,19 +1,45 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  ImageInput({super.key, required this.onPickImage});
 
+void Function(File image) onPickImage;
   @override
   State<ImageInput> createState() => _ImageInputState();
+
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _selectImage() {
+    File? selectedImage;
+  void _selectImage() async {
+    var imagePicker = ImagePicker();
+    var pickedImage = await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600 );
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      
+    selectedImage = File(pickedImage.path);
+    });
+    print(selectedImage!.path);
+    widget.onPickImage(selectedImage!);
   }
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: BoxDecoration(border: Border.all(color: Colors.white),), height: 200, child: Center(child: IconButton(onPressed: () {
+    Widget content = IconButton(icon: Icon(Icons.camera, color: Colors.white,),
+      onPressed: () {
     _selectImage();
-    }, icon: Icon(Icons.camera, color: Colors.white,)),),);
+    }, );
+    if (selectedImage != null) {
+      content = GestureDetector(
+        onTap: _selectImage,
+        child: Image.file(selectedImage!,
+          fit: BoxFit.cover, height: double.infinity,),
+      );
+    }
+    return Container(decoration: BoxDecoration(border: Border.all(color: Colors.white),), height: 200, child: Center(child: content,),);
   }
 }
